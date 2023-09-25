236894796723f95c98d1851fb718a9372681b55a
Vasilii Leontev <leovas1972@gmail.com>
2023-09-25 14:35:20 +0700

    added in test-selene

---
+++ tests/test_selene.py
@@ -0,0 +1,14 @@
+from selene import browser, by, be
+
+
+def test_github():
+    browser.open('/')
+
+    browser.element('.header-search-button').click()
+    browser.element('#query-builder-test').type('eroshenkoam/allure-example').press_enter()
+
+    browser.element(by.link_text('eroshenkoam/allure-example')).click()
+
+    browser.element('#issues-tab').click()
+    browser.element(by.partial_text('#76')).should(be.visible)
+
