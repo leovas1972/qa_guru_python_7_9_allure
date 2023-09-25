faf24ad49a63a393863f3fca771eb7c266233be7
Vasilii Leontev <leovas1972@gmail.com>
2023-09-25 14:32:49 +0700

    modified conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
-from selene.support.shared import browser
+from selene import browser
 from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser():
+def browser_config():
     browser.config.base_url = 'https://github.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
